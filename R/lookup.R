ee6e1becaf5bcaf34eb8ea3e5715ef30b498d20b
Clemens Schmid <clemens@nevrome.de>
2018-10-09 17:10:03 +0200

    nice value lookup code

--- R/lookup.R
+++ R/lookup.R
@@ -7,13 +7,64 @@
 #' 
 #' @export
 lookup_vars <- function(x, fb) {
-  
-  # geht relevant hash for fb
-  fb_hash <- hash::values(var_hash, fb)[[1]]
-  # lookup for variables in hash
+
+  # if no lookup possible than the input is returned
   res <- x 
+  
+  # check if there is a hash for this fb
+  if (!fb %in% hash::keys(var_hash)) {
+    return(res)
+  }  
+  
+  # get relevant hash for fb
+  fb_hash <- hash::values(var_hash, fb)[[1]]
+
+  # check which variables can be looked up
   vars_in_hash <- x %in% hash::keys(fb_hash)
+  
+  # if none can be looked up than the input is returned
+  if (!any(vars_in_hash)) {
+    return(res)
+  }
+  
+  # lookup for variables in hash
   res[vars_in_hash] <- hash::values(fb_hash, x[vars_in_hash])
   
   return(res)
 }
+
+#' lookup_attrs
+#'
+#' @param x Character Vector. Keys to look up values.
+#' @param vr Character. Relevant variable.
+#'
+#' @return Variable names.
+#' 
+#' @export
+lookup_attrs <- function(x, vr) {
+  
+  # if no lookup possible than the input is returned
+  res <- x 
+  
+  # check if there is a hash for this variable
+  if (!(vr %in% hash::keys(attr_hash))) {
+    return(res)
+  }
+  
+  # get relevant hash for vr
+  vr_hash <- hash::values(attr_hash, vr)[[1]]
+  
+  # check which variables can be looked up
+  attr_in_hash <- x %in% hash::keys(vr_hash)
+  
+  # if none can be looked up than the input is returned
+  if (!any(attr_in_hash)) {
+    return(res)
+  }
+  
+  # lookup for variables in hash
+  res[attr_in_hash] <- hash::values(vr_hash, x[attr_in_hash])
+  
+  return(res)
+}
+
--- playground/data_exploration.R
+++ playground/data_exploration.R
@@ -4,34 +4,4 @@ fb1 <- data.table::fread("../sdsmeta/example_data/Kuesterberg_fb1_test.csv", enc
 
 names(fb1) <- sdsanalysis::lookup_vars(names(fb1), 1)
 
-variable_values %<>%
-  dplyr::left_join(
-    variable_selection %>% dplyr::select(number, form_sheet_number, name_unified_de), 
-    by = c("number", "form_sheet_number")
-  )
-
-variable_values %>% dplyr::filter(form_sheet_number == 1) -> fb1_var_vals
-
-fb1_decoded <- fb1
-for (i in 1:ncol(fb1)) {
-  variable <- names(fb1)[i]
-  variable_data <- tibble::tibble(
-    X = as.character(unlist(fb1[,variable]))
-  )
-  variants <- fb1_var_vals[fb1_var_vals$name_unified_de == variable,]
-  fuzzy_variable <- dplyr::left_join(
-    variable_data,
-    variants,
-    by = c("X" = "Ausprägungsnr")
-  )$Ausprägungsname
-  variable_data[is.na(fuzzy_variable),] <- paste0("0", variable_data[is.na(fuzzy_variable),]$X)
-  fuzzy_variable <- dplyr::left_join(
-    variable_data,
-    variants,
-    by = c("X" = "Ausprägungsnr")
-  )$Ausprägungsname
-  fb1_decoded[i] <- fuzzy_variable
-  fb1_decoded[is.na(fb1_decoded[,i]),i] <- variable_data[is.na(fb1_decoded[,i]),1]
-}
-
-fb1_decoded
+fb1_decoded <- fb1 %>% purrr::map2_df(., names(.), .f = sdsanalysis::lookup_attrs)
