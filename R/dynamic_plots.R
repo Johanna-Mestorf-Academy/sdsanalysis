351cb3fc3f14b53477ad87b3d633ac7777a2ff55
Clemens Schmid <clemens@nevrome.de>
2018-10-15 11:19:59 +0200

    added a test prototype function to enable interactive and dynamic plots

---
+++ R/dynamic_plots.R
@@ -0,0 +1,21 @@
+#' dynamic_plot
+#'
+#' @param inputdata test
+#' @param var1 test
+#' @param var2 test
+#'
+#' @return ggplot object.
+#' 
+#' @export
+dynamic_plot <- function(inputdata, var1, var2) {
+  
+  ggplot(inputdata) +
+    geom_point(
+      aes_string(
+        x = var1,
+        y = var2
+      )
+    )
+    
+}
+
--- playground/data_exploration.R
+++ playground/data_exploration.R
@@ -4,3 +4,17 @@ x <- data.table::fread("../sdsmeta/example_data/Kuesterberg_fb1_test.csv", encod
 
 fb1_decoded <- lookup_everything(x, 1)
 
+fb <- fb1_decoded
+
+library(ggplot2)
+
+ggplot(fb) +
+  geom_point(
+    aes(dicke, groesse)
+  )
+
+inputdata <- fb
+var1 <- "fundjahr"
+var2 <- "gewicht"
+
+dynamic_plot(fb, "fundjahr", "gewicht")
