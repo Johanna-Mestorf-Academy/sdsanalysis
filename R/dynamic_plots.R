ef3ce8bd1edc2c2e30ab9d1d8fed0dafb4aaff8b
Clemens Schmid <clemens@nevrome.de>
2018-10-15 15:44:10 +0200

    missing dependency

--- R/dynamic_plots.R
+++ R/dynamic_plots.R
@@ -9,9 +9,9 @@
 #' @export
 dynamic_plot <- function(inputdata, var1, var2) {
   
-  ggplot(inputdata) +
-    geom_point(
-      aes_string(
+  ggplot2::ggplot(inputdata) +
+    ggplot2::geom_point(
+      ggplot2::aes_string(
         x = var1,
         y = var2
       )
