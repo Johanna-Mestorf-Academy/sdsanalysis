933f951f21115e78ba3aa3b1541ec89321414930
Clemens Schmid <clemens@nevrome.de>
2018-10-15 15:47:34 +0200

    removed new plotting function again -- no advantage compared to having all plotting code in the sdsbrowser repo

--- playground/data_exploration.R
+++ playground/data_exploration.R
@@ -3,18 +3,3 @@ library(magrittr)
 x <- data.table::fread("../sdsmeta/example_data/Kuesterberg_fb1_test.csv", encoding = "Latin-1") %>% tibble::as.tibble()
 
 fb1_decoded <- lookup_everything(x, 1)
-
-fb <- fb1_decoded
-
-library(ggplot2)
-
-ggplot(fb) +
-  geom_point(
-    aes(dicke, groesse)
-  )
-
-inputdata <- fb
-var1 <- "fundjahr"
-var2 <- "gewicht"
-
-dynamic_plot(fb, "fundjahr", "gewicht")
