00a767ecb7ffc4866e151f323ba2cc823d9cfa37
Clemens Schmid <clemens@nevrome.de>
2018-10-17 13:37:48 +0200

    some functions to get available datasets

---
+++ R/get_data.R
@@ -0,0 +1,41 @@
+#' get_data
+#'
+#' @param dataset_name Character. Name of an available dataset.
+#'
+#' @export
+get_data <- function(dataset_name) {
+  dataset_position_table <- lookup_data_positions(dataset_name)
+  dataset_fb_urls <- dataset_position_table$url[data_position$fb != "description"]
+  # to primitive: has to be adjusted to cover data composed of multiple fbs
+  dataset <- utils::read.csv(dataset_fb_urls, stringsAsFactors = FALSE, check.names = FALSE)
+  return(dataset)
+}
+
+#' get_description_HTML
+#'
+#' @param dataset_name Character. Name of an available dataset.
+#'
+#' @export
+get_description_HTML <- function(dataset_name) {
+  dataset_position_table <- lookup_data_positions(dataset_name)
+  dataset_description_url <- dataset_position_table$url[data_position$fb == "description"]
+  description <- readLines(dataset_description_url)
+  description_HTML <- paste(description, collapse = "<br>")
+  return(description_HTML)
+}
+
+#' lookup_data_positions
+#'
+#' @param dataset_name Character. Name of an available dataset.
+#'
+#' @export
+lookup_data_positions <- function(dataset_name) {
+  data_position[data_position$dataset == dataset_name, ]
+}
+
+#' get_available_datasets
+#'
+#' @export
+get_available_datasets <- function() {
+  unique(data_position$dataset)
+}
