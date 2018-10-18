ca17806f3353895e5913cd025c5b1e54a70c59eb
Clemens Schmid <clemens@nevrome.de>
2018-10-18 13:35:09 +0200

    added minimal data documentation

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' SDS Metadata
+#'
+#' See \url{https://github.com/nevrome/sdsanalysis/tree/master/data-raw/README.md}
+#'
+#' @rdname sds_metadata
+#' @name data_position
+NULL
+
+#' @rdname sds_metadata
+#' @name variable_values
+NULL
+
+#' @rdname sds_metadata
+#' @name variables
+NULL
