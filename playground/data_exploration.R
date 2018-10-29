08bda535934acdd2fc9ae909a16c121a230f01b4
Clemens Schmid <clemens@nevrome.de>
2018-10-29 16:14:54 +0100

    wrong function name in test code

--- playground/data_exploration.R
+++ playground/data_exploration.R
@@ -1,5 +1,5 @@
 library(magrittr)
 
-test_sds <- sdsanalysis::get_data("test_data")
+test_sds <- sdsanalysis::get_single_artefact_data("test_data")
 
 test_sds_decoded <- sdsanalysis::lookup_everything(test_sds)
