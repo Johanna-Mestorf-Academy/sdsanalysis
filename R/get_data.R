12d090d6fa560123b1af2c20ce3b49bfcfabf118
Clemens Schmid <clemens@nevrome.de>
2018-10-29 15:12:28 +0100

    some more adjustements of get_data code

--- R/get_data.R
+++ R/get_data.R
@@ -4,8 +4,8 @@
 #'
 #' @export
 get_single_artefact_data <- function(dataset_name) {
-  dataset_position_table <- lookup_data_positions(dataset_name)
-  dataset_url <- dataset_position_table$url[data_position$type == "single_artefacts"]
+  data_position <- lookup_data_positions(dataset_name)
+  dataset_url <- data_position$url[data_position$type == "single_artefacts"]
   dataset <- utils::read.csv(dataset_url, stringsAsFactors = FALSE, check.names = FALSE)
   return(dataset)
 }
@@ -16,8 +16,8 @@ get_single_artefact_data <- function(dataset_name) {
 #'
 #' @export
 get_multi_artefact_data <- function(dataset_name) {
-  dataset_position_table <- lookup_data_positions(dataset_name)
-  dataset_url <- dataset_position_table$url[data_position$type == "multi_artefacts"]
+  data_position <- lookup_data_positions(dataset_name)
+  dataset_url <- data_position$url[data_position$type == "multi_artefacts"]
   dataset <- utils::read.csv(dataset_url, stringsAsFactors = FALSE, check.names = FALSE)
   return(dataset)
 }
@@ -28,9 +28,9 @@ get_multi_artefact_data <- function(dataset_name) {
 #'
 #' @export
 get_description <- function(dataset_name) {
-  dataset_position_table <- lookup_data_positions(dataset_name)
-  dataset_description_url <- dataset_position_table$url[data_position$type == "description"]
-  description <- readLines(dataset_description_url)
+  data_position <- lookup_data_positions(dataset_name)
+  dataset_url <- data_position$url[data_position$type == "description"]
+  description <- readLines(dataset_url)
   return(description)
 }
 
@@ -40,7 +40,7 @@ get_description <- function(dataset_name) {
 #'
 #' @export
 lookup_data_positions <- function(dataset_name) {
-  data_position <- get_data_position_list() 
+  data_position <- get_data_positions() 
   data_position[data_position$dataset == dataset_name, ]
 }
 
@@ -48,16 +48,15 @@ lookup_data_positions <- function(dataset_name) {
 #'
 #' @export
 get_available_datasets <- function() {
-  data_position <- get_data_position_list() 
+  data_position <- get_data_positions() 
   unique(data_position$dataset)
 }
 
-#' get_data_position_list
+#' get_data_positions
 #' 
 #' @export
-get_data_position_list <- function(
-  pos = "https://raw.githubusercontent.com/Johanna-Mestorf-Academy/sdsanalysis/master/data-raw/data_position_list.csv"
-) {
+get_data_positions <- function() {
+  pos <- "https://raw.githubusercontent.com/Johanna-Mestorf-Academy/sdsanalysis/master/data-raw/data_position_list.csv"
   data_position <- utils::read.csv("data-raw/data_position_list.csv", stringsAsFactors = FALSE)
   return(data_position)
 }
