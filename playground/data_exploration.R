58c05a1a2a54915f21f2c1465f50bec1229f80b8
Clemens Schmid <clemens@nevrome.de>
2018-10-29 16:44:11 +0100

    experiments with test data

--- playground/data_exploration.R
+++ playground/data_exploration.R
@@ -1,5 +1,31 @@
 library(magrittr)
 
+#### package test data ####
+
 test_sds <- sdsanalysis::get_single_artefact_data("test_data")
 
 test_sds_decoded <- sdsanalysis::lookup_everything(test_sds)
+
+#### Kuesterberg ####
+
+test2 <- utils::read.csv("../sdsdata/Kuesterberg/Kuesterberg.csv", stringsAsFactors = F) %>% tibble::as.tibble()
+
+test2_decoded <- sdsanalysis::lookup_everything(test2)
+
+#### Bad-Oldesloe_Wolkenwehe #### 
+
+test_3 <- utils::read.csv(
+  "../sdsdata/Bad-Oldesloe_Wolkenwehe/Bad-Oldesloe_Wolkenwehe.csv",
+  stringsAsFactors = F
+) %>% tibble::as.tibble()
+
+test3_decoded <- sdsanalysis::lookup_everything(test_3)
+
+#### Oldenburg_Dannau ####
+
+test_4 <- utils::read.csv(
+  "../sdsdata/Oldenburg_Dannau/Oldenburg_Dannau_multi.csv",
+  stringsAsFactors = F
+) %>% tibble::as.tibble()
+
+test4_decoded <- sdsanalysis::lookup_everything(test_3)
