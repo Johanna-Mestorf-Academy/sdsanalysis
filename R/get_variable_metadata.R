d7e0b4b92e5e32f4a5565e692117aa7c9ac347c5
Clemens Schmid <clemens@nevrome.de>
2018-11-15 15:16:14 +0100

    building documentation

---
+++ R/get_variable_metadata.R
@@ -0,0 +1,13 @@
+#' get_variable_values_list
+#'
+#' @export
+get_variables_list <- function() {
+  return(variables)
+}
+
+#' get_variable_values_list
+#'
+#' @export
+get_variable_values_list <- function() {
+  return(variable_values)
+}
