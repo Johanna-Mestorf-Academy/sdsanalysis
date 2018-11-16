8a0e40c63bdd99d39df8d54f6dce0885c9923859
Clemens Schmid <clemens@nevrome.de>
2018-11-16 15:12:28 +0100

    algorithm to replace wide format in multi data to long format in single data

---
+++ playground/unmulti_single_data_brozio.R
@@ -0,0 +1,59 @@
+library(magrittr)
+
+sds <- read.csv(
+  "/home/clemens/sds/sdsdata/Bad-Oldesloe_Wolkenwehe/alt/Bad_Oldesloe-Wolkenwehe_LA154_single.csv",
+  stringsAsFactors = FALSE
+  )
+
+#### replace FB_19:FB_21 ####
+sds <- sds %>%
+  tidyr::gather(
+    FB1_35, value, FB7_19:FB7_21
+  ) %>%
+  dplyr::filter(
+    value != 0
+  ) %>%
+  dplyr:::select(
+    -value
+  ) %>%
+  dplyr::mutate(
+    FB1_35 = unname(sapply(
+      FB1_35,
+      function(x) {
+        switch (
+          x,
+          FB7_19 = 0,
+          FB7_20 = 1,
+          FB7_21 = 9
+        )
+      }
+    ))
+  )
+  
+#### replace FB_22:FB_27 ####
+sds <- sds %>%
+  tidyr::gather(
+    FB1_36, value, FB7_22:FB7_27
+  ) %>%
+  dplyr::filter(
+    value != 0
+  ) %>%
+  dplyr:::select(
+    -value
+  ) %>%
+  dplyr::mutate(
+    FB1_36 = unname(sapply(
+      FB1_36,
+      function(x) {
+        switch (
+          x,
+          FB7_22 = 0,
+          FB7_23 = 1,
+          FB7_24 = 2,
+          FB7_25 = 3,
+          FB7_26 = 4,
+          FB7_27 = 9
+        )
+      }
+    ))
+  )
